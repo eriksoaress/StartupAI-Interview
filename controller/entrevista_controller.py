ac82f7273515a5e53489f6bafe00b436235b6411
GustavoAntony <gustavoa9@al.insper.edu.br>
2024-04-25 09:25:24 -0300

    curriculo sendo lido

--- controller/entrevista_controller.py
+++ controller/entrevista_controller.py
@@ -1,7 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,File, UploadFile
+from PyPDF2 import PdfReader
+import io
 from service.entrevista_service import *
 from model.perguntas import *
 from model.avaliacao import *
+import uuid
+import io
+import pytesseract
+from PIL import Image
 
 
 entrevista_router = APIRouter( prefix="/entrevistas", tags=["entrevistas"])
@@ -13,3 +19,20 @@ def read_perguntas(perguntas: Pergunta):
 @entrevista_router.post("/respostas")
 def read_avaliacao(avaliacao: Avaliacao):
     return get_avaliacao(avaliacao.perguntas, avaliacao.respostas)
+
+@entrevista_router.post("/curriculo")
+async def read_curriculo(file: UploadFile = File(...)):
+    contents = await file.read()
+
+    
+    # Lendo o arquivo PDF
+    pdf_reader = PdfReader(io.BytesIO(contents))
+    num_pages = len(pdf_reader.pages)
+    text = ""
+
+    # Iterando através de cada página do PDF e extraindo o texto
+    for page_num in range(num_pages):
+        page = pdf_reader.pages[page_num]
+        text += page.extract_text()
+
+    return {"text": text}
