3fa78fa5cead3a7c6ca3b58a40c9753b57662fd0
eriksoaress <erikbtu2020@gmail.com>
2024-05-07 10:53:54 -0300

    db session controller

--- sql_app/controller/entrevista_controller.py
+++ sql_app/controller/entrevista_controller.py
@@ -1,20 +1,21 @@
-from fastapi import APIRouter,File, UploadFile
+from fastapi import APIRouter,File, UploadFile, Depends
 from PyPDF2 import PdfReader
 import io
 from service.entrevista_service import *
 import io
 from schemas import Entrevista, EntrevistaBase
+from database import get_db
 
 
 
 entrevista_router = APIRouter( prefix="/entrevistas", tags=["entrevistas"])
 
 @entrevista_router.post("/perguntas", response_model=Entrevista)
-async def read_perguntas(entrevista: EntrevistaBase, file: UploadFile = File(...)):
+async def read_perguntas(entrevista: EntrevistaBase, file: UploadFile = File(...), db: Session = Depends(get_db)):
     if not file.filename.endswith('.pdf'):
         return {"error": "Por favor, anexe um arquivo PDF"}
     contents = await file.read()
-    return get_perguntas(entrevista, contents)
+    return get_perguntas(entrevista, contents, db)
 
 # @entrevista_router.post("/respostas")
 # def read_avaliacao(avaliacao: EntrevistaBase):
