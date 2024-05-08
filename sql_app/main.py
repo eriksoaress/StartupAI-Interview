169d4539fdcf7ea83f18518ed14210b927fd76a4
GustavoAntony <gustavoa9@al.insper.edu.br>
2024-05-08 20:33:23 -0300

    cors adionado

--- sql_app/main.py
+++ sql_app/main.py
@@ -2,10 +2,17 @@ from fastapi import FastAPI
 from controller.entrevista_controller import *
 import models
 from database import engine
+from fastapi.middleware.cors import CORSMiddleware
 
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173/"],  # As origens que podem acessar este servidor
+    allow_credentials=True,
+    allow_methods=[""],  # Métodos HTTP permitidos
+    allow_headers=[""],  # Cabeçalhos HTTP permitidos
+)
 app.include_router(entrevista_router)
